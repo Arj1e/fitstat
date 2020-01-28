e1c704280132aed7405bcd4c99d23435f53b4d70
Adrian <aswiszcz@kamsoft.pl>
2020-01-28 14:48:08 +0100

    added close params_panel function, TODO: Obtain problem with close animation

--- Components/ParamsPanel/ParamsPanel.js
+++ Components/ParamsPanel/ParamsPanel.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react'
+import {connect} from 'react-redux';
+import cycle_actions from '../../Ducks/LifeCycle/actions';
 import './style.css';
 
 class ParamsPanel extends Component {
+    closeParamsPanel=()=>{
+        this.props.set_mask_animation("unmount");
+    }
     render() {
         return (
             <div className={'params-panel'}>
-                test
+            <div className={'close-params'} onClick={this.closeParamsPanel} >+</div>    
             </div>
         )
     }
 }
+const mapStateToProps= state =>({
+    cyclestate: state.cyclestate
+})
+const mapDispatchToProps = dispach =>({
+    set_mask_animation : cycle => dispach(cycle_actions.maskanimate(cycle))
+    
+})
 
-export default ParamsPanel;
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+                        )(ParamsPanel);
+
