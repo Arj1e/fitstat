58ec7e310954bd189fff634f92277ce3aee6ed7c
Adrian <aswiszcz@kamsoft.pl>
2020-01-28 14:14:41 +0100

    support mask mount and unmount in App component

--- Components/App/App.js
+++ Components/App/App.js
@@ -4,26 +4,34 @@ import LoginPanel from '../LoginPanel/LoginPanel';
 import MainButton from '../MainButton/MainButton';
 import Mask from '../Mask/Mask';
 import { connect } from 'react-redux';
-class App extends React.Component{
-  
-    render(){
-        if(this.props.cyclestate.login_panel_cycle === "unmount" ){
-           return(
-               <div>
-               <Mask/>
-               <MainButton/>
-               </div>
-           )
-        }else{
-            return(
-                <LoginPanel/>
+class App extends React.Component {
+
+    render() {
+        if (this.props.cyclestate.login_panel_cycle === "unmount") {
+            if (this.props.cyclestate.mask_params_cycle == "unmount") {
+                return (
+                    <div>
+                        <MainButton />
+                    </div>
+                )
+            } else {
+                return (
+                    <div>
+                        <Mask />
+                        <MainButton />
+                    </div>
+                )
+            }
+        } else {
+            return (
+                <LoginPanel />
             )
         }
     }
 }
-const mapStateToProps= state =>({
+const mapStateToProps = state => ({
     appstate: state.appstate,
     cyclestate: state.cyclestate
 })
 
-export default connect(mapStateToProps,{})(App);
+export default connect(mapStateToProps, {})(App);
